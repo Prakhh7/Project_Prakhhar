9b5228fe313c9796bddf0f6b2938e61abe996f5b
Prakhh7 <prakhhartandon7@gmail.com>
2019-04-22 19:52:17 +0530

    Second Commit

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 int main()
 {
+cout<<"Press 1 for Stack and Queues\n"<<"Press 2 to draw a circle\n"<<"Press 3 to draw a rectangle";
+int choice;
+cin>>choice;
+switch(choice)
+{
+case 1:
+break ;
+
+case 2:
+circle();
+break ;
+
+case 3:
+rectangle();
+break ;
+}
 return 0;
 }
